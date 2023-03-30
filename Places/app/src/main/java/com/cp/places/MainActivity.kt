7eb496ebbc3361665bbd27d28d268876aa326a68
Chetanpatel57 <40812586+Chetanpatel57@users.noreply.github.com>
2023-03-30 10:27:44 -0400

    Initial commit

---
+++ Places/app/src/main/java/com/cp/places/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cp.places
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
